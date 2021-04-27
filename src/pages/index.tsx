f579559242400653c223fe25e7f82c8e52bf46e8
Ozan Yıldız <developer@yildizozan.com>
2021-04-27 11:58:31 +0300

    Everything are renewed

---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+import { FC } from 'react'
+
+import Creditcard from 'components/CreditCard'
+
+const HomePage: FC = () => {
+  return (
+    <div className="container p-8">
+      <Creditcard />
+    </div>
+  )
+}
+
+export default HomePage
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,11 @@
 module.exports = {
-  purge: ['./pages/**/*.js', './components/**/*.js'],
-  darkMode: false,
-  variants:{
-    extend: {
-      opacity: ["disabled"]
-    }
-  }
+  purge: ['./src/pages/**/*.{js,ts,jsx,tsx}', './src/components/**/*.{js,ts,jsx,tsx}'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {}
+  },
+  variants: {
+    extend: {}
+  },
+  plugins: []
 }
